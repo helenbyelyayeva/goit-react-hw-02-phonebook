be12b342304c2c3cc2addac8db8937ff78db5157
Helen Byelyayeva <91469540+helenbyelyayeva@users.noreply.github.com>
2023-05-09 20:28:08 +0300

    p2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,67 @@
 import React, { Component } from "react";
 import { ContactForm } from "./ContactsForm/ContactForm";
 import { ContactList } from "./ContactsList/ContactList";
+import { Filter } from "./Search/Filter";
+import css from "./App.module.css";
+import { nanoid } from "nanoid";
+
 export class App extends Component {
   state = {
-    contacts: [],
-  //   name: '',
-  //   number: ''
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    ],
+    filter:''
   }
 
-  deleteContact = contactId => {
+addContact = ({ name, number }) => {
+    const isAdded = this.state.contacts.some(contact => contact.name === name);
+
+    if (isAdded) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
     this.setState(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== contactId),
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
+
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  deleteContact = id => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== id),
     }));
   };
 
   render() {
-   return (
-    <div>   
-     <h2>Phonebook</h2>
-    <ContactForm></ContactForm>
-    <ContactList
-    //  contacts={contacts}
-     onDelete={this.deleteContact.bind(this)}
-    />
- 
-    </div>
+    const filteredContacts = this.getFilteredContacts();
+    return (
+      <div  className={css.form}>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2 className={css.subtitle}>Contacts</h2>
+        <Filter onChange={this.changeFilter} />
+        <ContactList
+          contacts={filteredContacts}
+          onDelete={this.deleteContact}
+        />
+      </div>
     );
   }
 }
--- src/components/ContactsForm/ContactForm.jsx
+++ src/components/ContactsForm/ContactForm.jsx
@@ -29,38 +29,38 @@ export class ContactForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit.bind(this)}>
+            <form onSubmit={this.handleSubmit.bind(this)} className={css.form}>
                 <label className={css.formLabel}>
                     Name
-                    <input
-                        id={this.nameId}
-                        className={css.formInput}
-                        type="text"
-                        name="name"
-                        value={this.name}
-                        placeholder="Please enter name"
-                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required
-                        onChange={this.handelChange.bind(this)}
-                    />
                 </label>
+                <input
+                    id={this.nameId}
+                    className={css.formInput}
+                    type="text"
+                    name="name"
+                    value={this.name}
+                    placeholder="Please enter name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                    onChange={this.handelChange}
+                />
                 <label className={css.formLabel}>
                     Phone number
-                    <input
-                        id={this.numberId}
-                        className={css.formInput}
-                        type="tel"
-                        name="number"
-                        value={this.number}
-                        placeholder="Please enter number"
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                        required
-                        onChange={this.handelChange.bind(this)}
-                    />
                 </label>
-                <button type="submit">Add contact</button>
+                <input
+                    id={this.numberId}
+                    className={css.formInput}
+                    type="tel"
+                    name="number"
+                    value={this.number}
+                    placeholder="Please enter number"
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    onChange={this.handelChange}
+                />
+                <button className={css.btn} type="submit">Add contact</button>
             </form>
         )
     }
